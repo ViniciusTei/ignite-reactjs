7c2f4e21abd1c780ac8424644d5f30dc2ea07f4c
Vincius <viniciustprates@gmail.com>
2021-05-12 10:50:34 -0300

    feat add transaction type buttons

--- vt-money/src/components/NewTransactionModal/index.tsx
+++ vt-money/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
 import Modal from 'react-modal';
+import { Container, TransactionTypeContainer } from './styles';
 
-import { Container } from './styles';
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
@@ -25,6 +27,17 @@ export const NewTransactionModal = ({isOpen, onRequestClose}: NewTransactionModa
             <h2>Cadastrar transação</h2>
             <input type="text" placeholder="Titulo" />
             <input type="number" placeholder="Valor" />
+
+            <TransactionTypeContainer>
+                <button type="button">
+                    <img src={incomeImg} alt="Entrada" />
+                    <span>Entradas</span>
+                </button>
+                <button type="button">
+                    <img src={outcomeImg} alt="Saida" />
+                    <span>Saidas</span>
+                </button>
+            </TransactionTypeContainer>
             <input type="text" placeholder="Categoria" />
             <button type="submit">Cadastrar</button>
         </Container>
--- vt-money/src/components/NewTransactionModal/styles.ts
+++ vt-money/src/components/NewTransactionModal/styles.ts
@@ -45,3 +45,37 @@ export const Container = styled.form`
         }
     }
 `;
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: .5rem;
+
+    button {
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: .25rem;
+        background: transparent;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: border-color .2s;
+        &:hover {
+            border-color: #aaa;
+        }
+
+        img {
+            width: 28px;
+            height: 28px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
+        }
+    }
+`
