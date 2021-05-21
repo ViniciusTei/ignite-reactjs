474808e148b6e36607cebada9512efdfdfc433bb
Vincius <viniciustprates@gmail.com>
2021-05-21 09:55:40 -0300

    started implemantation of header

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+export const Header: React.FC = () => {
+  return (
+      <header className={styles.headerContainer}>
+          <div className={styles.headerrContent}>
+              <img src="/images/logo.svg" alt="Logo ig.news" />
+              <nav>
+                  <a className={styles.active} href="">Home</a>
+                  <a href="">Posts</a>
+              </nav>
+          </div>
+      </header>
+  );
+}
