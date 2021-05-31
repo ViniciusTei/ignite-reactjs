9ca177fbd4a7aab707c8da5f7a3257b07d8d8225
Vincius <viniciustprates@gmail.com>
2021-05-31 11:18:49 -0300

    💥started connection with database

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -10,6 +13,24 @@ export default NextAuth({
       scope: 'read:user'
     }),
     // ...add more providers here
-  ]
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      } catch (error) {
+        return false
+      }
+      
+    }
+  }
 
 })
