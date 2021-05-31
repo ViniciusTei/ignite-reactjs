3377131047ea2611c438763f5fddf013a2632880
Vincius <viniciustprates@gmail.com>
2021-05-31 10:40:37 -0300

    ✨add authentication with next auth adn github

--- ignews/src/components/SigninButton/index.tsx
+++ ignews/src/components/SigninButton/index.tsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SigninButton() {
-    const isUserLoggedIn = false;
-
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.siginButton}>
+    const [session] = useSession()
+    
+    return session ? (
+        <button 
+            type="button" 
+            className={styles.siginButton} 
+            onClick={() => signOut()}
+        >
             <FaGithub color="#04d361"/>
             Vinicius Teixeira
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
-        <button type="button" className={styles.siginButton}>
+        <button 
+            type="button" 
+            className={styles.siginButton}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417"/>
             Sigin with github
         </button>
--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import { AppProps } from 'next/app';
-
+import { Provider } from 'next-auth/client'
 import { Header } from '../components/Header';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
 
