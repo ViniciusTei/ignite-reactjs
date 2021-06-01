425fac38a737522ad17624a29430507d11548bba
Vincius <viniciustprates@gmail.com>
2021-06-01 13:13:52 -0300

    ✨add stripe to the project

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import { useSession, signIn } from 'next-auth/client'
 
 import styles from './styles.module.scss'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession()
+    
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button onClick={handleSubscribe} type="button" className={styles.subscribeButton}>
             Subscribe now
         </button>
     )
--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client'
 import { Header } from '../components/Header';
+
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
     <Provider session={pageProps.session}>
       <Header />
