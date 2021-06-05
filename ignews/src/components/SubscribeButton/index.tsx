6e11b683e3eab61a9b62f70c6568299ca9fa653b
Vincius <viniciustprates@gmail.com>
2021-06-05 09:44:46 -0300

    🔒 validation of active subscriptions

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -4,16 +4,23 @@ import { useSession, signIn } from 'next-auth/client'
 import styles from './styles.module.scss'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js'
+import { useRouter } from 'next/router'
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const [session] = useSession()
+    const router = useRouter()
     
     async function handleSubscribe() {
         if(!session) {
             signIn('github')
+            return
+        }
+
+        if(session.activeSubscription) {
+            router.push('/posts')
             return
         }
 
