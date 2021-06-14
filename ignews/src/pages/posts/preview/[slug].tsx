332edb48364ff1fbfa33ec07e8cfc151583a2594
Vincius <viniciustprates@gmail.com>
2021-06-14 09:24:27 -0300

    removed wrong depencies

--- ignews/src/pages/posts/preview/[slug].tsx
+++ ignews/src/pages/posts/preview/[slug].tsx
@@ -79,6 +79,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         props: {
             post
-        }
+        },
+        redirect: 60 * 30, // 30 minutos
     }
 }
