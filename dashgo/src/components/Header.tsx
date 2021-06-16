f96ba3ad21d9f5477e10338101ca395e6e0edd95
Vincius <viniciustprates@gmail.com>
2021-06-16 16:49:04 -0300

    ✨ created header

---
+++ dashgo/src/components/Header.tsx
@@ -0,0 +1,50 @@
+import { Flex, Input, Text, Icon } from '@chakra-ui/react';
+import { RiSearchLine } from 'react-icons/ri';
+
+export function Header() {
+    return(
+        <Flex
+            as="header"
+            w="100%"
+            maxW={1480}
+            h="20"
+            marginX="auto"
+            mt="4"
+            align="center"
+            px="6"
+            >
+            <Text
+                fontSize="3xl"
+                fontWeight="bold"
+                letterSpacing="tight"
+                w="64"
+            >dashgo
+            <Text as="span" ml="1" color="pink.500">.</Text>
+            </Text>
+
+            <Flex
+                as="label"
+                flex="1"
+                py="4"
+                px="8"
+                ml="6"
+                maxW={400}
+                alignSelf="center"
+                color="gray.200"
+                position="relative"
+                bg="gray.800"
+                borderRadius="full"
+            >
+                <Input 
+                    color="gray.50"
+                    variant="unstyled"
+                    placeholder="Buscar na plataforma"
+                    _placeholder={{color: 'gray.400'}}
+                    px="4"
+                    mr="4"
+                />
+                <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+        </Flex>
+    )
+}
