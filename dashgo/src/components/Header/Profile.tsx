29969570ac58f617d8016942118b9f589d0afdff
Vincius <viniciustprates@gmail.com>
2021-06-23 08:00:21 -0300

    add responsivity

--- dashgo/src/components/Header/Profile.tsx
+++ dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData: boolean
+}
+
+export function Profile({showProfileData}: ProfileProps) {
     return (
         <Flex
             align="center"
         >
+            {showProfileData && 
             <Box mr="4" align="right">
                 <Text>Vinicius Teixeira</Text>
                 <Text color="gray.300" fontSize="small">viniciustprates@gmail.com</Text>
             </Box>
+            }
             <Avatar size="md" name="Vinicius Teixeira" src="https://github.com/viniciustei.png"></Avatar>
         </Flex>
     )
